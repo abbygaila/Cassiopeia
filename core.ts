dc981185eb201fad4121074807e8d0cb2a478884
Veronica <Werixisia@yandex.com>
2021-12-30 22:45:40 +0100

    Initial commit

---
+++ core.ts
@@ -0,0 +1,24 @@
+import Discord, { Intents, MessageReaction } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Greetings starlight~!')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.ClientPasscode)
